3ad8e47b0b1fae961d909804e8953fee3d11f201
Bahou <Bahou@Aissam>
2015-09-21 12:13:41 +0200

    Commit before synchronization

---
+++ src/application/RobotApplication.java
@@ -0,0 +1,50 @@
+package application;
+
+
+import com.kuka.roboticsAPI.applicationModel.RoboticsAPIApplication;
+import static com.kuka.roboticsAPI.motionModel.BasicMotions.*;
+import com.kuka.roboticsAPI.controllerModel.Controller;
+import com.kuka.roboticsAPI.deviceModel.LBR;
+import com.kuka.roboticsAPI.geometricModel.Tool;
+
+/**
+ * Implementation of a robot application.
+ * <p>
+ * The application provides a {@link RoboticsAPITask#initialize()} and a 
+ * {@link RoboticsAPITask#run()} method, which will be called successively in 
+ * the application lifecycle. The application will terminate automatically after 
+ * the {@link RoboticsAPITask#run()} method has finished or after stopping the 
+ * task. The {@link RoboticsAPITask#dispose()} method will be called, even if an 
+ * exception is thrown during initialization or run. 
+ * <p>
+ * <b>It is imperative to call <code>super.dispose()</code> when overriding the 
+ * {@link RoboticsAPITask#dispose()} method.</b> 
+ * 
+ * @see #initialize()
+ * @see #run()
+ * @see #dispose()
+ */
+public class RobotApplication extends RoboticsAPIApplication {
+	private Controller controller;
+	private LBR robot;
+	private Tool UsedTool;
+
+	public void initialize() {
+		controller = (Controller) getContext().getControllers().toArray()[0];
+		robot = (LBR) controller.getDevices().toArray()[0];
+		UsedTool = getApplicationData().createFromTemplate("UsedTool");
+		UsedTool.attachTo(robot.getFlange());
+				}
+
+	public void run() {
+		
+	}
+
+	/**
+	 * Auto-generated method stub. Do not modify the contents of this method.
+	 */
+	public static void main(String[] args) {
+		RobotApplication app = new RobotApplication();
+		app.runApplication();
+	}
+}
