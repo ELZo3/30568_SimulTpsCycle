12df657df47231fe55c06409f9f5499fd775df87
Bahou <Bahou@Aissam>
2015-09-22 11:31:46 +0200

    Commit before synchronization

--- src/application/RobotApplication.java
+++ src/application/RobotApplication.java
@@ -75,20 +75,20 @@ public class RobotApplication extends RoboticsAPIApplication {
 		// Saving the first frame
 		UsedTool.getFrame("TCP").move(linRel(0,200,0).setJointJerkRel(0.2).setCartVelocity(80).breakWhen(Y_contact));
 		P1.setX(robot.getCurrentCartesianPosition(UsedTool.getFrame("TCP"), World.Current.getRootFrame()).getX());
-		P1.setY(robot.getCurrentCartesianPosition(UsedTool.getFrame("TCP"), World.Current.getRootFrame()).getY());
+		P1.setZ(robot.getCurrentCartesianPosition(UsedTool.getFrame("TCP"), World.Current.getRootFrame()).getZ());
 		System.out.println("First position saved !");
 		// Saving the second frame
 		UsedTool.getFrame("TCP").moveAsync(linRel(-30,-20,0).setCartVelocity(160));
 		UsedTool.getFrame("TCP").move(linRel(0,200,0).setJointJerkRel(0.2).setCartVelocity(80).breakWhen(Y_contact));
 		P2.setX(robot.getCurrentCartesianPosition(UsedTool.getFrame("TCP"), World.Current.getRootFrame()).getX());
-		P2.setY(robot.getCurrentCartesianPosition(UsedTool.getFrame("TCP"), World.Current.getRootFrame()).getY());
+		P2.setZ(robot.getCurrentCartesianPosition(UsedTool.getFrame("TCP"), World.Current.getRootFrame()).getZ());
 		System.out.println("Second position saved !");
 		// Saving the third frame
 		UsedTool.getFrame("TCP").moveAsync(linRel(-150,-30,0).setCartVelocity(160).setBlendingRel(0.1));
 		UsedTool.getFrame("TCP").moveAsync(linRel(0,80,0).setCartVelocity(160));
 		UsedTool.getFrame("TCP").move(linRel(200,0,0).setJointJerkRel(0.2).setCartVelocity(80).breakWhen(X_contact));
 		P3.setX(robot.getCurrentCartesianPosition(UsedTool.getFrame("TCP"), World.Current.getRootFrame()).getX());
-		P3.setY(robot.getCurrentCartesianPosition(UsedTool.getFrame("TCP"), World.Current.getRootFrame()).getY());
+		P3.setZ(robot.getCurrentCartesianPosition(UsedTool.getFrame("TCP"), World.Current.getRootFrame()).getZ());
 		System.out.println("Third position saved !");
 		
 		Recaler_base rb = new Recaler_base();
