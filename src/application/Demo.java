65c9d1f9c1ba734efdb0a152a61613ad3bdbdc47
Bahou <Bahou@Aissam>
2015-09-22 09:57:57 +0200

    Commit before synchronization

---
+++ src/application/Demo.java
@@ -0,0 +1,105 @@
+package application;
+
+
+import java.util.concurrent.TimeUnit;
+
+import com.kuka.roboticsAPI.applicationModel.RoboticsAPIApplication;
+import static com.kuka.roboticsAPI.motionModel.BasicMotions.*;
+
+import com.kuka.roboticsAPI.conditionModel.ForceCondition;
+import com.kuka.roboticsAPI.controllerModel.Controller;
+import com.kuka.roboticsAPI.deviceModel.JointPosition;
+import com.kuka.roboticsAPI.deviceModel.LBR;
+import com.kuka.roboticsAPI.geometricModel.CartDOF;
+import com.kuka.roboticsAPI.geometricModel.Frame;
+import com.kuka.roboticsAPI.geometricModel.Tool;
+import com.kuka.roboticsAPI.geometricModel.math.CoordinateAxis;
+import com.kuka.roboticsAPI.motionModel.controlModeModel.CartesianImpedanceControlMode;
+
+/**
+ * Implementation of a robot application.
+ * <p>
+ * The application provides a {@link RoboticsAPITask#initialize()} and a 
+ * {@link RoboticsAPITask#run()} method, which will be called successively in 
+ * the application lifecycle. The application will terminate automatically after 
+ * the {@link RoboticsAPITask#run()} method has finished or after stopping the 
+ * task. The {@link RoboticsAPITask#dispose()} method will be called, even if an 
+ * exception is thrown during initialization or run. 
+ * <p>
+ * <b>It is imperative to call <code>super.dispose()</code> when overriding the 
+ * {@link RoboticsAPITask#dispose()} method.</b> 
+ * 
+ * @see #initialize()
+ * @see #run()
+ * @see #dispose()
+ */
+public class Demo extends RoboticsAPIApplication {
+	private Controller controller;
+	private LBR robot;
+	private Tool UsedTool;
+	private ForceCondition X_contact;
+	private ForceCondition Y_contact;
+	private ForceCondition Z_contact;
+	private CartesianImpedanceControlMode souple;
+	private CartesianImpedanceControlMode rigide;
+	
+	
+	public void initialize() {
+		controller = (Controller) getContext().getControllers().toArray()[0];
+		robot = (LBR) controller.getDevices().toArray()[0];
+		UsedTool = getApplicationData().createFromTemplate("UsedTool");
+		UsedTool.attachTo(robot.getFlange());
+		X_contact = ForceCondition.createNormalForceCondition(UsedTool.getFrame("TCP"), CoordinateAxis.X, 5);
+		Y_contact = ForceCondition.createNormalForceCondition(UsedTool.getFrame("TCP"), CoordinateAxis.Y, 5);
+		Z_contact = ForceCondition.createNormalForceCondition(UsedTool.getFrame("TCP"), CoordinateAxis.Z, 5);
+		souple = new CartesianImpedanceControlMode();
+		souple.parametrize(CartDOF.TRANSL).setStiffness(150);
+		
+		rigide = new CartesianImpedanceControlMode();
+		rigide.parametrize(CartDOF.TRANSL).setStiffness(5000);
+		
+		
+				}
+
+	public void run() {
+		
+		JointPosition start = new JointPosition(
+				Math.toRadians(42.48),
+				Math.toRadians(8.10),
+				Math.toRadians(0),
+				Math.toRadians(-86.51),
+				Math.toRadians(0),
+				Math.toRadians(84),
+				Math.toRadians(27.45)
+				);
+		
+		robot.move(ptp(start).setJointVelocityRel(0.5));
+		
+		System.out.println("Detection de contact selon X");
+		UsedTool.getFrame("TCP").move(linRel().setXOffset(100).breakWhen(X_contact));
+		robot.move(ptp(start).setJointVelocityRel(0.5));
+		
+		System.out.println("Detection de contact selon Y");
+		UsedTool.getFrame("TCP").move(linRel().setYOffset(100).breakWhen(X_contact));
+		robot.move(ptp(start).setJointVelocityRel(0.5));
+		
+		System.out.println("Detection de contact selon Z");
+		UsedTool.getFrame("TCP").move(linRel().setZOffset(100).breakWhen(X_contact));
+		robot.move(ptp(start).setJointVelocityRel(0.5));
+		
+		System.out.println("Garder la position en mode rigide");
+		UsedTool.getFrame("TCP").move(positionHold(rigide, 2, TimeUnit.MINUTES));
+		System.out.println("Garder la position en mode souple");
+		UsedTool.getFrame("TCP").move(positionHold(souple, 2, TimeUnit.MINUTES));
+		
+		
+	}
+
+	/**
+	 * Auto-generated method stub. Do not modify the contents of this method.
+	 */
+	public static void main(String[] args) {
+		Demo app = new Demo();
+		app.runApplication();
+	}
+}
--- src/baseCalculation/Recaler_base.java
+++ src/baseCalculation/Recaler_base.java
@@ -15,48 +15,48 @@ public class Recaler_base {
 		// The equation of this line is (D): ax+by+c=0
 		// We will calculate the vector that contains the following variables a,b and c
 		
-		Vector x_vector = new Vector(0,0,0);
+		Vector Z_vector = new Vector(0,0,0);
 		
-		x_vector.setA(P2.getY()-P1.getY());			// a
-		x_vector.setB(-(P2.getX()-P1.getX()));		// b
+		Z_vector.setA(P2.getX()-P1.getX());			// a
+		Z_vector.setB(-(P2.getZ()-P1.getZ()));		// b
 		
 		// c is the result of the scalar product of (a,b) vector and (P1.getX, P1.getY) vector
 		
 		Vector V_P1 = new Vector(P1.getX(), P1.getY(), 0);
-		Vector u = new Vector(x_vector.getA(),x_vector.getB(),0);
-		x_vector.setC(-u.dotProduct(V_P1)); //c
+		Vector u = new Vector(Z_vector.getA(),Z_vector.getB(),0);
+		Z_vector.setC(-u.dotProduct(V_P1)); //c
 		
 		//--------(The perpendicular line to (D) that contains P3)--------------//
 		// The equation of this line is (D'): a'x+b'y+c'=0
 		// We will calculate the vector that contains the following variables a',b' and c'
 		
-		Vector y_vector = new Vector(0,0,0);
-		y_vector.setA(P2.getX()-P1.getX());		// a'
-		y_vector.setB(P2.getY()-P1.getY());		// b'
+		Vector X_vector = new Vector(0,0,0);
+		X_vector.setA(P2.getZ()-P1.getZ());		// a'
+		X_vector.setB(P2.getX()-P1.getX());		// b'
 		
 		//c' is the result of the scalar product of (a',b') and (P3.getX, P3.getY) vector
 		Vector V_P3 = new Vector(P3.getX(), P3.getY(), 0);
-		Vector v = new Vector(y_vector.getA(),y_vector.getB(),0);
-		y_vector.setC(-v.dotProduct(V_P3)); //c'
+		Vector v = new Vector(X_vector.getA(),X_vector.getB(),0);
+		X_vector.setC(-v.dotProduct(V_P3)); //c'
 		
 		//--------------(Origin Calculation = Intersection of (D) and (D'))--------------//
 		// In order to have the origin, we must solve a system of the two equations (D) and (D')
 		// Thus we need to define determinant
 		
-		double det = (x_vector.getA()*y_vector.getB())-(y_vector.getA()*x_vector.getB());
-		double detx= (y_vector.getC()*x_vector.getB())-(x_vector.getC()*y_vector.getB());
-		double dety= (y_vector.getA()*x_vector.getC())-(x_vector.getA()*y_vector.getC());
+		double det = (Z_vector.getA()*X_vector.getB())-(X_vector.getA()*Z_vector.getB());
+		double detx= (X_vector.getC()*Z_vector.getB())-(Z_vector.getC()*X_vector.getB());
+		double dety= (X_vector.getA()*Z_vector.getC())-(Z_vector.getA()*X_vector.getC());
 		
-		Base.setX(detx/det);
-		Base.setY(dety/det);
+		Base.setZ(detx/det);
+		Base.setX(dety/det);
 		
-		if(Base.getY()==P1.getY())
+		if(Base.getX()==P1.getX())
 		{
 			return Base;
 		}
 		else
 		{
-			Base.setAlphaRad(Base.getAlphaRad()+Math.atan((Base.getY()-P1.getY())/((Base.getX()-P1.getX()))));
+			Base.setAlphaRad(Base.getAlphaRad()+Math.atan((Base.getX()-P1.getX())/((Base.getZ()-P1.getZ()))));
 			return Base;
 		}
 		
