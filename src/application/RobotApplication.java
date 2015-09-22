9cb30b2e6c5e9b986e7a21a58cbcc4da7d9338a5
Bahou <Bahou@Aissam>
2015-09-22 12:23:59 +0200

    Commit before synchronization

--- src/application/RobotApplication.java
+++ src/application/RobotApplication.java
@@ -99,7 +99,7 @@ public class RobotApplication extends RoboticsAPIApplication {
 		Recaler_base rb = new Recaler_base();
 		AbstractFrame new_base = rb.calcul_base(P1, P2, P3, getApplicationData().getFrame("/Process"));
 		UsedTool.getFrame("TCP").moveAsync(linRel(-20,0,0).setCartVelocity(160));
-		UsedTool.getFrame("TCP").moveAsync(linRel(0,0,-130).setCartVelocity(160));
+		UsedTool.getFrame("TCP").moveAsync(linRel(50,50,-130).setCartVelocity(160));
 		//UsedTool.getFrame("TCP").move(ptp(new_base).setJointVelocityRel(0.1));
 		
 		
