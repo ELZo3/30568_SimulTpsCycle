10b9d31f3b857af14ababf762a3dd67abd05de3d
Tarlet <Tarlet@KUKA-TARLET>
2015-10-13 17:33:46 +0200

    Commit avant synchronisation

--- src/application/CT.java
+++ src/application/CT.java
@@ -38,9 +38,9 @@ public class CT extends RoboticsAPIApplication {
 
 	public void run() {
 		lbr_iiwa_14_R820_1.move(ptpHome());
-		lbr_iiwa_14_R820_1.move(ptp(Math.toRadians(50),0,0,0,0,0,0).setJointVelocityRel(0.5));
+		lbr_iiwa_14_R820_1.move(ptp(Math.toRadians(50),0,0,0,0,0,0));
 		lbr_iiwa_14_R820_1.move(ptp(0,0,0,Math.toRadians(-100),0,0,0));
-		lbr_iiwa_14_R820_1.move(ptpHome());
+		lbr_iiwa_14_R820_1.move(ptpHome().setJointVelocityRel(0.2));
 
 	}
 
