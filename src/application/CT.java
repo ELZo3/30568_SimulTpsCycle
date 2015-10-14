f247d1adaf51b37458844314464c1ad268cc5d55
GIRARD <GIRARD@KUKA-GIRARDR>
2015-10-14 14:11:35 +0200

    Commit avant synchronisation

--- src/application/CT.java
+++ src/application/CT.java
@@ -44,8 +44,8 @@ public class CT extends RoboticsAPIApplication {
 		//lbr_iiwa_14_R820_1.move(ptpHome());
 		lbr_iiwa_14_R820_1.move(ptp(getApplicationData().getFrame("/Version1robot1/A1")));
 		lbr_iiwa_14_R820_1.move(ptp(getApplicationData().getFrame("/Version1robot1/A2")));
-		lbr_iiwa_14_R820_1.move(ptp(getApplicationData().getFrame("/Version1robot1/A5")));
-		lbr_iiwa_14_R820_1.move(ptp(getApplicationData().getFrame("/Version1robot1/A6")));
+		lbr_iiwa_14_R820_1.move(ptp(getApplicationData().getFrame("/Version1robot1/A5"))); // attention changer à 448.3
+		lbr_iiwa_14_R820_1.move(ptp(getApplicationData().getFrame("/Version1robot1/A6"))); // attention changer à 448.3
 		
 		
 		//lbr_iiwa_14_R820_1.move(ptp(getApplicationData().getFrame("/TC/P1")));
